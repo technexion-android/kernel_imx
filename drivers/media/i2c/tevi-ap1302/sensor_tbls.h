ea88bb6e9fedc2b0097f24f819ba6f3754bdb5f7
JerryLiu <jerry.liu@technexion.com>
2023-07-11 10:40:53 +0800

    media: tevi-ap1302: remove useless gpios and add sensor mode for all resolutions of sensors.

--- drivers/media/i2c/tevi-ap1302/sensor_tbls.h
+++ drivers/media/i2c/tevi-ap1302/sensor_tbls.h
@@ -1,56 +1,63 @@
 #ifndef __AP1302_SENSOR_TABLES_H__
 #define __AP1302_SENSOR_TABLES_H__
 
-struct resolution {
+struct sensor_params {
 	u16 width;
 	u16 height;
 	u16 framerates;
+	u16 mode;
 };
 
-static struct resolution ar0144_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 60},
-	{.width = 1280, .height = 800, .framerates = 60},
+static struct sensor_params ar0144_res_list[] = {
+	{.width = 640, .height = 480, .framerates = 60, .mode = 0},
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 0},
+	{.width = 1280, .height = 800, .framerates = 60, .mode = 0},
 };
 
-static struct resolution ar0234_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 100},
-	{.width = 1920, .height = 1080, .framerates = 100},
-	{.width = 1920, .height = 1200, .framerates = 100},
+static struct sensor_params ar0234_res_list[] = {
+	{.width = 640, .height = 480, .framerates = 120, .mode = 1},
+	{.width = 1280, .height = 720, .framerates = 120, .mode = 0},
+	{.width = 1280, .height = 800, .framerates = 60, .mode = 0},
+	{.width = 1920, .height = 1080, .framerates = 85, .mode = 0},
+	{.width = 1920, .height = 1200, .framerates = 60, .mode = 0},
 };
 
-static struct resolution ar0521_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},
-	{.width = 1920, .height = 1080, .framerates = 30},
-	{.width = 2592, .height = 1944, .framerates = 30},
+static struct sensor_params ar0521_res_list[] = {
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
+	{.width = 1920, .height = 1080, .framerates = 60, .mode = 1},
+	{.width = 2560, .height = 1440, .framerates = 40, .mode = 1},
+	{.width = 2592, .height = 1944, .framerates = 40, .mode = 1},
 };
 
-static struct resolution ar0821_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},
-	{.width = 1920, .height = 1080, .framerates = 30},
-	{.width = 2560, .height = 1440, .framerates = 30},
-	{.width = 3840, .height = 2160, .framerates = 25},
+static struct sensor_params ar0522_res_list[] = {
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 3},
+	{.width = 1920, .height = 1080, .framerates = 60, .mode = 1},
+	{.width = 2560, .height = 1440, .framerates = 40, .mode = 1},
+	{.width = 2592, .height = 1944, .framerates = 40, .mode = 1},
 };
 
-static struct resolution ar1335_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},    //HD  720p
-	{.width = 1920, .height = 1080, .framerates = 30},   //FHD 1080p
-	{.width = 2560, .height = 1440, .framerates = 30},   //2K  1440p
-	{.width = 3840, .height = 2160, .framerates = 30},   //4K  2160p
-	{.width = 4192, .height = 3120, .framerates = 20},
-	{.width = 4208, .height = 3120, .framerates = 20},
+static struct sensor_params ar0821_res_list[] = {
+	{.width = 1280, .height = 720, .framerates = 30, .mode = 1},
+	{.width = 1920, .height = 1080, .framerates = 30, .mode = 1},
+	{.width = 2560, .height = 1440, .framerates = 30, .mode = 0},   //2K  1440p
+	{.width = 3840, .height = 2160, .framerates = 24, .mode = 0},
 };
 
-static struct resolution ar1820_res_list[] = {
-	{.width = 1280, .height = 720, .framerates = 30},    //HD  720p
-	{.width = 1920, .height = 1080, .framerates = 30},   //FHD 1080p
-	{.width = 2560, .height = 1440, .framerates = 30},   //2K  1440p
-	{.width = 3840, .height = 2160, .framerates = 30},   //4K  2160p
-	{.width = 4136, .height = 3102, .framerates = 20},
+static struct sensor_params ar1335_res_list[] = {
+	{.width = 640, .height = 480, .framerates = 60, .mode = 4},     //VGA  480p
+	{.width = 1280, .height = 720, .framerates = 60, .mode = 4},    //HD  720p
+	{.width = 1920, .height = 1080, .framerates = 60, .mode = 3},   //FHD 1080p
+	{.width = 2560, .height = 1440, .framerates = 30, .mode = 1},   //2K  1440p
+	{.width = 3840, .height = 2160, .framerates = 20, .mode = 0},   //4K  2160p
+	// {.width = 4192, .height = 3120, .framerates = 24},
+	// {.width = 4208, .height = 3120, .framerates = 15},
 };
 
 struct sensor_info {
 	const char* sensor_name;
-	const struct resolution *res_list;
+	const struct sensor_params *res_list;
 	u32 res_list_size;
 };
 
@@ -72,8 +79,8 @@ static struct sensor_info ap1302_sensor_table[] = {
 	},
 	{
 		.sensor_name = "TEVI-AR0522",
-		.res_list = ar0521_res_list,
-		.res_list_size = ARRAY_SIZE(ar0521_res_list)
+		.res_list = ar0522_res_list,
+		.res_list_size = ARRAY_SIZE(ar0522_res_list)
 	},
 	{
 		.sensor_name = "TEVI-AR0821",
@@ -81,14 +88,14 @@ static struct sensor_info ap1302_sensor_table[] = {
 		.res_list_size = ARRAY_SIZE(ar0821_res_list)
 	},
 	{
+		.sensor_name = "TEVI-AR0822",
+		.res_list = ar0821_res_list,
+		.res_list_size = ARRAY_SIZE(ar0821_res_list)
+	},
+	{
 		.sensor_name = "TEVI-AR1335",
 		.res_list = ar1335_res_list,
 		.res_list_size = ARRAY_SIZE(ar1335_res_list)
-	},
-	{
-		.sensor_name = "TEVI-AR1820",
-		.res_list = ar1820_res_list,
-		.res_list_size = ARRAY_SIZE(ar1820_res_list)
 	},
 };
 
