c6ff3fa4e9531a1412f4ae0d294209237d81faec
Joe Zhang <joe.zhang@technexion.com>
2024-12-18 10:13:02 +0800

    sec-dsim: call panel pm_runtime_en(dis)able before panel (un)prepare
    workaround for panel-simple-dsi is not the right state of pm_runtime.
    Still not found who run 'pm_runtime_disable' somewhere else.

--- drivers/gpu/drm/bridge/sec-dsim.c
+++ drivers/gpu/drm/bridge/sec-dsim.c
@@ -1373,6 +1373,8 @@ sec_mipi_dsim_bridge_atomic_enable(struct drm_bridge *bridge,
 
 	/* prepare panel if exists */
 	if (dsim->panel) {
+		if (!pm_runtime_enabled(dsim->panel->dev))
+			pm_runtime_enable(dsim->panel->dev);
 		ret = drm_panel_prepare(dsim->panel);
 		if (unlikely(ret)) {
 			dev_err(dsim->dev, "panel prepare failed: %d\n", ret);
@@ -1473,6 +1475,8 @@ disable:
 
 	/* unprepare panel if exists */
 	if (dsim->panel) {
+		if (pm_runtime_enabled(dsim->panel->dev))
+			pm_runtime_disable(dsim->panel->dev);
 		ret = drm_panel_unprepare(dsim->panel);
 		if (unlikely(ret))
 			dev_err(dsim->dev, "panel unprepare failed: %d\n", ret);
