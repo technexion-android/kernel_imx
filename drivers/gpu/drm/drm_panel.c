75b56c466e67815792a66f0b1e3a48dd57424961
Tifa Chen <tifa.chen@technexion.com>
2025-04-08 18:42:20 -0700

    gpu: drm: Workarond for enable lvds panel on imx8mm
    On Android 14, dispaly HAL will cause lvds panel init failed.
    This is workaround to skip enable panel until display HAL start.

--- drivers/gpu/drm/drm_panel.c
+++ drivers/gpu/drm/drm_panel.c
@@ -32,6 +32,10 @@
 static DEFINE_MUTEX(panel_lock);
 static LIST_HEAD(panel_list);
 
+#ifdef CONFIG_DRM_8MM_WA
+static int enable_cnt = 0;
+#endif
+
 /**
  * DOC: drm panel
  *
@@ -113,6 +117,10 @@ int drm_panel_prepare(struct drm_panel *panel)
 	struct drm_panel_follower *follower;
 	int ret;
 
+#ifdef CONFIG_DRM_8MM_WA
+	if (enable_cnt < 1)
+		return 0;
+#endif
 	if (!panel)
 		return -EINVAL;
 
@@ -216,6 +224,12 @@ int drm_panel_enable(struct drm_panel *panel)
 {
 	int ret;
 
+#ifdef CONFIG_DRM_8MM_WA
+	if(enable_cnt < 1) {
+		enable_cnt += 1;
+		return 0;
+	}
+#endif
 	if (!panel)
 		return -EINVAL;
 
@@ -254,6 +268,11 @@ int drm_panel_disable(struct drm_panel *panel)
 {
 	int ret;
 
+#ifdef CONFIG_DRM_8MM_WA
+	if(enable_cnt == 1) {
+		return 0;
+	}
+#endif
 	if (!panel)
 		return -EINVAL;
 
