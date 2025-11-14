e509a0b57c9db225504700faffc4c32d4cab7613
Nian Yang <nian.yang@technexion.com>
2025-11-14 18:12:17 +0800

    media: tevs: remove bayer raw support

--- drivers/media/i2c/tevs/tevs_tbls.h
+++ drivers/media/i2c/tevs/tevs_tbls.h
@@ -31,7 +31,6 @@ static struct resolution ar0144_res_list[] = {
 
 static u32 ar0144_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG12_1X12,
 };
 
 /* AR0145 default setting for 4 data lanes and data frequency 800 MHz */
@@ -45,7 +44,6 @@ static struct resolution ar0145_res_list[] = {
 
 static u32 ar0145_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG10_1X10,
 };
 
 /* AR0234 default setting for 4 data lanes and data frequency 800 MHz */
@@ -60,7 +58,6 @@ static struct resolution ar0234_res_list[] = {
 
 static u32 ar0234_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG10_1X10,
 };
 
 /* AR0521 default setting for 4 data lanes and data frequency 800 MHz */
@@ -75,8 +72,6 @@ static struct resolution ar0521_res_list[] = {
 
 static u32 ar0521_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG10_1X10,
-	MEDIA_BUS_FMT_SGRBG12_1X12,
 };
 
 /* AR0522 default setting for 4 data lanes and data frequency 800 MHz */
@@ -91,8 +86,6 @@ static struct resolution ar0522_res_list[] = {
 
 static u32 ar0522_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG10_1X10,
-	MEDIA_BUS_FMT_SGRBG12_1X12,
 };
 
 /* AR0821 default setting for 4 data lanes and data frequency 800 MHz */
@@ -106,8 +99,6 @@ static struct resolution ar0821_res_list[] = {
 
 static u32 ar0821_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG12_1X12,
-	MEDIA_BUS_FMT_SGRBG16_1X16,
 };
 
 /* AR0822 default setting for 4 data lanes and data frequency 800 MHz */
@@ -121,8 +112,6 @@ static struct resolution ar0822_res_list[] = {
 
 static u32 ar0822_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG12_1X12,
-	MEDIA_BUS_FMT_SGRBG16_1X16,
 };
 
 /* AR1335 default setting for 4 data lanes and data frequency 800 MHz */
@@ -137,7 +126,6 @@ static struct resolution ar1335_res_list[] = {
 
 static u32 ar1335_code_list[] = {
 	MEDIA_BUS_FMT_UYVY8_1X16,
-	MEDIA_BUS_FMT_SGRBG10_1X10,
 };
 
 struct sensor_info {
